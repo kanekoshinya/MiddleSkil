f780568b6eac23ac0dc1bd90c6bf09f0a3d11a14
kanekoshinya <60633085+kanekoshinya@users.noreply.github.com>
2023-04-12 12:07:27 +0900

    テストコミット

--- 1-9/src/test1.java
+++ 1-9/src/test1.java
@@ -3,18 +3,16 @@ public class test1 {
 
 	public static void main(String[] args) {
 		
-//		1.数字の5と9の変数を定義し、足した数字を出力しなさい。
-		int i = 5;
-		int a = 9;
-		System.out.println(i+a);
-		
-//		2.64ビット整数の「1111111111」を定義し2倍した値を出力しなさい。
-		long f = 1111111111;
-		System.out.println(f*2);
-		
-//		3.boolean型の変数の値に不正な文字列比較を入れてfalseを出力してください。
-		boolean r = "aa".equals("ii");
-		System.out.println(r);
+		 String a = "";
+		 int x = -1;
+         int y = 80;
+         int z = 15;
+         
+         a = (x < 0 && y < 0 && z < 0) ? (x % 10 == 0 && y % 10== 0 && z % 10== 0)? 
+          		"正の整数：NG" : "10の倍数：NG" 
+          		        : ( x + y + z == 100) ? 
+          		        "プラス100：OK" : "プラス100：NG";
+          		System.out.println(a); 
 		
 	}
 
